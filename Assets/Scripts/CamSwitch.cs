afb4308c0e4dd57a8aa39b56ad2a498e0944a5d1
Nalina-Rai <np03cs4s240096@heraldcollege.edu.np>
2024-07-29 14:06:20 +0545

    Added wheelprayers

---
+++ Assets/Scripts/CamSwitch.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwitch : MonoBehaviour
+{
+   public GameObject cam1,cam2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("1Key"))
+        {
+            cam1.SetActive(true);
+            cam2.SetActive(false);
+        }
+
+        if (Input.GetButtonDown("2Key"))
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+        }
+
+
+
+    }
+}
